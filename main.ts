7336ead7a979a6913e8e36759b2ec5e283b4f0f0
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-12 13:10:29 -0400

    linting fix

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Sep 2023
+ * This program prints "Hello, World!" on a Micro:bit
 */
 
 basic.showString('Hello, World!')
